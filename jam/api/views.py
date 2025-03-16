56063ff815948ed7c714dacbce0b134bd7e8258f
Shanthan  Sudhini <162046574+Shanthan2307@users.noreply.github.com>
2025-03-15 20:26:15 -0500

    added working apiview

--- jam/api/views.py
+++ jam/api/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
 from .serializers import Roomserializer
-import .models from Room
+from .models import Room
 # Create your views here.
 
-class RoomView(generics.CreateAPIView):
-    queryset = Room.ojects.all()
+class RoomView(generics.ListCreateAPIView):
+    queryset = Room.objects.all()
     serializer_class = Roomserializer
