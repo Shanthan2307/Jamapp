18718c3425d4adc8eeb017f25438eee52112e4ab
lakshkumar06 <laksh150034@gmail.com>
2025-03-16 16:33:45 -0500

    added components

--- jam/frontend/src/App.jsx
+++ jam/frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RoomJoinPage from "./components/RoomJoinPage";
+import CreateRoomPage from "./components/CreateRoomPage";
+import Room from "./components/Room";
 
 const App = () => {
   return (
     <div>
-      <h1 className="">Hello 1 world</h1>
+      <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/join" element={<RoomJoinPage />} />
+        <Route path="/create" element={<CreateRoomPage />} />
+        <Route path="/room/:roomCode" element={<Room />} />
+      </Routes>
+    </Router>
     </div>
   )
 }
---
+++ jam/frontend/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <p className="">Home Page</p>
+    </div>
+  )
+}
+
+export default HomePage
